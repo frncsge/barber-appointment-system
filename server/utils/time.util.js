c2d869f112f6a19ac5c81cce19f4dcbe1160c210
Francis Ge <francisge527@gmail.com>
2026-03-24 01:40:40 +0800

    add file for time related reusable functions

---
+++ server/utils/time.util.js
@@ -0,0 +1,45 @@
+export const validateTime = (time) => {
+  const timePattern = /^([01]\d|2[0-3]):([0-5]\d)$/;
+
+  return timePattern.test(time);
+};
+
+export const parseTime = (time) => {
+  return time.split(":").map((str) => Number(str));
+};
+
+export const validateWorkHour = (startTime, endTime) => {
+  const [startHour, startMin] = parseTime(startTime);
+  const [endHour, endMin] = parseTime(endTime);
+
+  // invalid if end hour is earlier than start hour
+  if (startHour > endHour || (startHour === endHour && startMin >= endMin)) {
+    return false;
+  }
+
+  return true;
+};
+
+export const validateSlotInterval = (slotInterval) => {
+  const parsedSlotInterval = Number(slotInterval);
+  if (isNaN(parsedSlotInterval) || parsedSlotInterval <= 0) {
+    return false;
+  }
+
+  return true;
+};
+
+export const validateSlotIntervalLength = ({
+  startTime,
+  endTime,
+  slotInterval,
+}) => {
+  const [startHour, startMin] = parseTime(startTime);
+  const [endHour, endMin] = parseTime(endTime);
+  const startTotal = startHour * 60 + startMin;
+  const endTotal = endHour * 60 + endMin;
+
+  if (endTotal - startTotal < slotInterval) return false;
+
+  return true;
+};
