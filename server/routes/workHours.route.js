5be56c2c5adaab81e58b2c61c8390cf9fd9fa8ac
Francis Ge <francisge527@gmail.com>
2026-03-27 13:17:10 +0800

    add route for getting time time slots by date

--- server/routes/workHours.route.js
+++ server/routes/workHours.route.js
@@ -3,10 +3,12 @@ import {
   addWorkHours,
   getWorkHours,
 } from "../controllers/workHours.controller.js";
+import { getAvailableTimeSlots } from "../controllers/workHours.controller.js";
 
 const router = express.Router();
 
 router.post("/work-hours", addWorkHours);
 router.get("/work-hours/:date", getWorkHours);
+router.get("/work-hours/:date/time-slots", getAvailableTimeSlots);
 
 export default router;
