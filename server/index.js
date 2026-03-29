da4d2be762dbf8080cf9669b7f6cec88ebd90544
Francis Ge <francisge527@gmail.com>
2026-03-29 14:44:59 +0800

    use auth routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import workHoursRoutes from "./routes/workHours.route.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -9,6 +10,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+app.use("/api", authRoutes);
 app.use("/api", workHoursRoutes);
 
 app.get("/", (req, res) => {
