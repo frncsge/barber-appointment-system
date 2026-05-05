afd7211331cd50505b0d9c54b8d0907520f471f4
Francis Ge <francisge527@gmail.com>
2026-05-06 01:48:37 +0800

    feat: add middleware for global rate limiting

--- server/middlewares/rateLimit.middleware.js
+++ server/middlewares/rateLimit.middleware.js
@@ -1,49 +1,19 @@
-import redisClient from "../../config/redisConfig.js";
+import { checkRateLimit } from "../utils/rateLimit.util.js";
 
-export const createRateLimit = ({ getRules, window }) => {
-  return async (req, res, next) => {
-    try {
-      const rules = getRules(req);
+export const globalRateLimit = async (req, res, next) => {
+  try {
+    const allowRequests = await checkRateLimit(
+      [{ key: `rateLimit:global:ip:${req.ip}`, maxAttempts: 100 }],
+      60,
+    );
 
-      if (!rules || rules.length === 0) return next();
-
-      const results = await Promise.all(
-        rules.map(async (rule) => {
-          const count = await redisClient.incr(rule.key);
-
-          if (count === 1) await redisClient.expire(rule.key, window);
-
-          return { ...rule, count };
-        }),
-      );
-
-      const exceeded = results.some(
-        ({ count, maxAttempts }) => count > maxAttempts,
-      );
-
-      if (exceeded)
-        return res
-          .status(429)
-          .json({ message: "Too many attempts. Please try again later." });
-
-      next();
-    } catch (error) {
-      console.error("Rate limit error:", err);
-      next();
+    if (!allowRequests) {
+      return res.status(429).json({ message: "Too many request" });
     }
-  };
-};
 
-export const loginRateLimit = createRateLimit({
-  getRules: (req) => [
-    {
-      key: `rateLimit:login:ip:${req.ip}`,
-      maxAttempts: 15,
-    },
-    {
-      key: `rateLimit:login:email:${req.body.email?.trim().toLowerCase()}`,
-      maxAttempts: 5,
-    },
-  ],
-  window: 15 * 60,
-});
+    next();
+  } catch (error) {
+    console.error("Global rate limit error:", error);
+    next();
+  }
+};
