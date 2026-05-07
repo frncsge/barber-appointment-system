acf8964ca31e4e80afdb9bd47ce474a69f16fa03
Francis Ge <francisge527@gmail.com>
2026-05-07 22:20:37 +0800

    feat: add functions for creating and getting appointments from the database

---
+++ server/models/appointments.model.js
@@ -0,0 +1,37 @@
+import pool from "../../config/dbConfig.js";
+
+export const getAppointments = async (barberId, date) => {
+  try {
+    const result = await pool.query(
+      "SELECT * FROM appointments WHERE user_id = $1 AND date = $2",
+      [barberId, date],
+    );
+
+    return result.rows;
+  } catch (error) {
+    console.error("An error occured while trying to get appointments:", error);
+    throw error;
+  }
+};
+
+export const createAppointment = async ({
+  barberId,
+  date,
+  timeSlot,
+  customerName,
+}) => {
+  try {
+    await pool.query("INSERT INTO appointments VALUES ($1, $2, $3, $4)", [
+      date,
+      timeSlot,
+      customerName,
+      barberId,
+    ]);
+  } catch (error) {
+    console.error(
+      "An error occured while trying to create an appointment:",
+      error,
+    );
+    throw error;
+  }
+};
