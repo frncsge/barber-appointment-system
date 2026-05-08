c5d4ff7cb474ba3c1029dda506d2583004926840
Francis Ge <francisge527@gmail.com>
2026-05-08 17:40:51 +0800

    feat: add route for getting appointments by date

--- server/routes/appointments.route.js
+++ server/routes/appointments.route.js
@@ -1,7 +1,14 @@
 import express from "express";
-import { bookAppointment } from "../controllers/appointments.controller.js";
+import {
+  bookAppointment,
+  getAppointments,
+} from "../controllers/appointments.controller.js";
+import { authenticateUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+
+// for getting all appointments or by a specific date
+router.get("/appointments", authenticateUser, getAppointments);
 
 // for customers to book an appointment on a specific date
 router.post("/barbers/:id/appointments", bookAppointment);
