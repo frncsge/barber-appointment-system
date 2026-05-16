ede9514353ca7a8e3e837891e9b5b93bb41553c7
Francis Ge <francisge527@gmail.com>
2026-05-16 12:08:22 +0800

    feat: add route for getting user info (me)

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -93,14 +93,14 @@ export const logIn = async (req, res) => {
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "lax",
       maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "Strict",
+      sameSite: "lax",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
