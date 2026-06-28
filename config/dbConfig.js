e92fbae28c1b460fa1fab00a27484f42b0767228
Francis Ge <francisge527@gmail.com>
2026-06-29 01:53:47 +0800

    refactor: database connection from local pgsql to supabase

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -4,11 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new pg.Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export default pool;
